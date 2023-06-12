e6f9a4d17b28a8060622eb2801d8f3c37599922b
arpita2105 <136358528+arpita2105@users.noreply.github.com>
2023-06-12 22:36:40 +0530

    Add files via upload

---
+++ Task 4/script.js
@@ -0,0 +1,24 @@
+
+   var swiper = new Swiper(".course-slider", {
+        spaceBetween:20,
+        garbCursor:true,
+        loop:true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable:true,
+        },
+        breakpoints:{
+            540: {
+                slidesPerView:1,
+            },
+            768: {
+                slidesPerView:2,
+            },
+            1024: {
+                slidesPerView:3,
+            },
+        },
+    });
+
+
+    
